4e16acde513e5272b7c775109503f0c15c3481d2
Serhii <158744795+BurglachOK@users.noreply.github.com>
2025-03-15 19:20:52 +0100

    done

--- get_cats_info.py
+++ get_cats_info.py
@@ -1,4 +1,11 @@
 
+def get_cats_info(path):
+    cats_info = []
+    with open(path, encoding='utf-8') as file:
+        for line in file:
+            id, name, age = line.strip().split(',')
+            cats_info.append({"id": id, "name": name, "age": age})
+    return cats_info
 
 
 
@@ -12,11 +19,20 @@
 
 
 
+cats_info = get_cats_info("cats_file.txt")
+print(cats_info)
 
 
 
+# Очікуваний результат:
 
-
+# [
+#     {"id": "60b90c1c13067a15887e1ae1", "name": "Tayson", "age": "3"},
+#     {"id": "60b90c2413067a15887e1ae2", "name": "Vika", "age": "1"},
+#     {"id": "60b90c2e13067a15887e1ae3", "name": "Barsik", "age": "2"},
+#     {"id": "60b90c3b13067a15887e1ae4", "name": "Simon", "age": "12"},
+#     {"id": "60b90c4613067a15887e1ae5", "name": "Tessi", "age": "5"},
+# ]
 
 
 
